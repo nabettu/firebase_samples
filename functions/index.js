0e962da75447cf0ea6eb033c2cb0202dd390fd7d
nabettu <tatsuaki.w@gmail.com>
2017-09-16 17:38:02 +0900

    シェア機能追加

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+const fs = require("fs");
+
+exports.share = functions.https.onRequest((req, res) => {
+    res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    res.set('Vary', 'Accept-Encoding, X-My-Custom-Header');
+
+    const id = req.query.id;
+    const domain = req.protocol + '://' + req.headers.host;
+
+    let title = 'firebase samples';
+    let description = 'firebase samples on website';
+    let shareUrl = 'https://nabettu-firebase-sample.firebaseapp.com';
+    let ogpImage = '';
+    let redirectUrl = shareUrl;
+
+    admin
+        .database()
+        .ref(`users/${id}`)
+        .once('value')
+        .then(dbData => {
+            const user = dbData.val()
+            if(user){
+                title = user.name;
+                description = user.description;
+                shareUrl = domain + '/share/?id=' + id;
+                ogpImage = user.photoURL;
+                redirectUrl = domain + '/user/?id=' + id;
+            }
+            const templeteHtml = fs.readFileSync('./templete.html', 'UTF-8');
+            const responseHtml = templeteHtml
+                .replace(/\$title/g, title)
+                .replace(/\$description/g, description)
+                .replace(/\$shareUrl/g, shareUrl)
+                .replace(/\$ogpImage/g, ogpImage)
+                .replace(/\$redirectUrl/g, redirectUrl);
+            res.status(200).send(responseHtml);
+        });
+});
